d49e6d012e88b9c1ce28eb49fc4ebc3ceed15dd7
Abdel-Malek-Sellou <abdelmalek.sellou@edu.esiee.fr>
2025-09-15 14:44:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from math import sqrt
 
 
 def isprime(p):
+     d = 2 #on commence par diviser par 2 et on monte petit à petit
+    ispremier = True #on part du principe que le nombre est premier jusqu'à preuve du contraire
 
-    # votre code ici
+    while d < p:
+        if p % d == 0:
+            q = p // d #on définit le quotient pour pouvoir l'appeler dans le print
+            ispremier = False #le nombre n'est pas premier
+        d += 1
+    return(ispremier)
 
     pass
 
