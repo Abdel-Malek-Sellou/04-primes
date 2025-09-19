824fce932f26f6b73ad6c517bf62c038188f9c40
Abdel-Malek-Sellou <abdelmalek.sellou@edu.esiee.fr>
2025-09-19 09:41:14 +0200

    main.py

--- main.py
+++ main.py
@@ -33,3 +33,20 @@ def isprime(p):
             ispremier = False
         d += 1
     return ispremier
+    pass
+    
+ #### Fonction principale
+
+def main():
+
+    # vos appels à la fonction secondaire ici
+
+    for n in range(100):
+        if isprime(n):
+            print(n, end=", ")
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
